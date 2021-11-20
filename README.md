6aa99b66c121c10a73ed2aa3f9a621b9cdc761cd
GodPaa <1539699463@qq.com>
2021-11-20 23:23:53 +0800

    修改readme.md

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # vue-springbooot-demo
 整合Springboot+Vue，实现了登录功能和对一个表的CRUD
+
+
+
+
+
+1. 登录界面
+
+![image-20211120231215799](C:\Users\admin\AppData\Roaming\Typora\typora-user-images\image-20211120231215799.png)
+
+2. 后台管理界面
+
+   ![image-20211120231702907](C:\Users\admin\AppData\Roaming\Typora\typora-user-images\image-20211120231702907.png)
+
+3. 主要实现的功能
+
+     对单表crud
+
+     分页
+
+     登录功能
+
+4. 用到的技术（框架）
+
+   后台：
+
+   ​	springboot
+
+   ​	mybatis-plus
+
+   前端：
+
+   ​    vue3
+
+   ​	axios
+
+   ​    
+
+   
--- element-ui/demo/src/utils/request.js
+++ element-ui/demo/src/utils/request.js
@@ -12,24 +12,24 @@ const whiteUrls = ["/user/login", '/user/register']
 // request 拦截器
 // 可以自请求发送前对请求做一些处理
 // 比如统一加token，对请求参数统一加密
-request.interceptors.request.use(config => {
-    config.headers['Content-Type'] = 'application/json;charset=utf-8';
-
-    // 取出sessionStorage里面缓存的用户信息
-    let userJson = sessionStorage.getItem("user")
-
-    if (!whiteUrls.includes(config.url)) {  // 校验请求白名单
-        if(!userJson) {
-            router.push("/login")
-        } else {
-            let user = JSON.parse(userJson);
-            config.headers['token'] = user.token;  // 设置请求头
-        }
-    }
-    return config
-}, error => {
-    return Promise.reject(error)
-});
+// request.interceptors.request.use(config => {
+//     config.headers['Content-Type'] = 'application/json;charset=utf-8';
+//
+//     // 取出sessionStorage里面缓存的用户信息
+//     let userJson = sessionStorage.getItem("user")
+//
+//     if (!whiteUrls.includes(config.url)) {  // 校验请求白名单
+//         if(!userJson) {
+//             router.push("/login")
+//         } else {
+//             let user = JSON.parse(userJson);
+//             config.headers['token'] = user.token;  // 设置请求头
+//         }
+//     }
+//     return config
+// }, error => {
+//     return Promise.reject(error)
+// });
 
 // response 拦截器
 // 可以在接口响应后统一处理结果
